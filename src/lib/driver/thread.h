0e4c0eefa7578dbb38f6361aa88320694b42d32f
Rohit Jalan <r0h17@purpe.com>
2021-12-08 15:14:01 +0530

    lib/driver

---
+++ src/lib/driver/thread.h
@@ -0,0 +1,29 @@
+#ifndef __SUMERU_THREAD_H
+#define __SUMERU_THREAD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "thread_machine_state.h"
+
+typedef struct __attribute__((__packed__)) thread
+{
+    /* crt0 expects tms to be at offset 0 */
+    thread_machine_state_t	tms;
+
+    unsigned int	priority;
+
+    struct thread	*runq_next;
+    struct thread	*runq_prev;
+
+    struct thread	*t_next;
+    struct thread	*t_prev;
+
+} thread_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
